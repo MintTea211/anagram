6e7a5342bb63c81a1ed917e15a16b87fcd9dacda
l0852 <l0852@pjwstk.edu.pl>
2024-03-07 10:40:55 +0100

    poczatek kodu,wczytywanie danych

--- zadanie/src/Main.java
+++ zadanie/src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+//1.
+//Wczytywanie danych
+        try {
+            FileReader fr = new FileReader("C:\\Users\\l0852\\Downloads");
+            BufferedReader br = new BufferedReader(fr);
+            ArrayList<String> linie=new ArrayList<>(1000);
+            String line=br.readLine();
+            while(line!=null){
+                linie.add(line);
+                line= br.readLine();
+                System.out.println(line);
+            }
+            System.out.println("Size "+linie.size());
+            br.close();
+        }catch (FileNotFoundException exc){
+            System.out.println("Nie znaleziono pliku");
+        }catch(IOException exc){
+            System.out.println("Plik jest uzywany przez inny proces");
         }
+
     }
 }
