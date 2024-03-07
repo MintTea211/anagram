4221f51dcd2034a85cf09f902b999a474961a159
l0852 <l0852@pjwstk.edu.pl>
2024-03-07 11:11:37 +0100

    metoda

--- zadanie/src/Main.java
+++ zadanie/src/Main.java
@@ -4,8 +4,11 @@ public class Main {
     public static void main(String[] args) {
 //1.
 //Wczytywanie danych
+
+
+
         try {
-            FileReader fr = new FileReader("C:\\Users\\l0852\\Downloads");
+            FileReader fr = new FileReader("C:\\Users\\l0852\\Downloads\\slowa.txt");
             BufferedReader br = new BufferedReader(fr);
             ArrayList<String> linie=new ArrayList<>(1000);
             String line=br.readLine();
@@ -16,11 +19,33 @@ public class Main {
             }
             System.out.println("Size "+linie.size());
             br.close();
+
         }catch (FileNotFoundException exc){
             System.out.println("Nie znaleziono pliku");
         }catch(IOException exc){
             System.out.println("Plik jest uzywany przez inny proces");
         }
 
+
+    }
+
+    private static ArrayList<String> WczytajDane(String path) {
+        try {
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            ArrayList<String> linie=new ArrayList<>(1000);
+            String line=br.readLine();
+            while(line!=null){
+                linie.add(line);
+                line= br.readLine();
+            }
+            br.close();
+            return linie;
+        }catch (FileNotFoundException exc){
+            System.out.println("Nie znaleziono pliku");
+        }catch(IOException exc){
+            System.out.println("Plik jest uzywany przez inny proces");
+        }
+        return null;
     }
 }
